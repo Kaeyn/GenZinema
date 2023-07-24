3b3494024370c1bce7039004f968076c0972480c
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2023-07-24 15:21:27 +0700

    Add HotnNew Fragment

--- app/src/main/java/android/genzinema/Controller/Cus_Item_Search_Adapter.java
+++ app/src/main/java/android/genzinema/Controller/Cus_Item_Search_Adapter.java
@@ -1,5 +1,8 @@
 package android.genzinema.Controller;
 
+import android.genzinema.Model.Movie;
+import android.genzinema.R;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -9,27 +12,33 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class Cus_Item_Search_Adapter extends RecyclerView.Adapter<Cus_Item_Search_Adapter.MyViewHolder> {
+
+    ArrayList<Movie> arrayListMovie = new ArrayList<>();
 
     @NonNull
     @Override
-    public Cus_Item_Search_Adapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public Cus_Item_Search_Adapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_layout_item_search, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Cus_Item_Search_Adapter.MyViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull Cus_Item_Search_Adapter holder, int position) {
-
+        Movie movie = arrayListMovie.get(position);
+        holder.textView.setText(movie.getNameMovie());
+        holder.imageButton.setImageResource(R.drawable.play_icon);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayListMovie.size();
+    }
+
+    public Cus_Item_Search_Adapter(ArrayList<Movie> arrayListMovie){
+        this.arrayListMovie = arrayListMovie;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
@@ -39,6 +48,9 @@ public class Cus_Item_Search_Adapter extends RecyclerView.Adapter<Cus_Item_Searc
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imageItem_Search);
+            textView = itemView.findViewById(R.id.textItem_Search);
+            imageButton = itemView.findViewById(R.id.btnToMovie_Search);
         }
     }
 }
---
+++ app/src/main/java/android/genzinema/View/Fragment_HotnNew.java
@@ -0,0 +1,77 @@
+package android.genzinema.View;
+
+import android.genzinema.Controller.Hot_New_Pager_Adapter;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.genzinema.R;
+
+import com.google.android.material.tabs.TabLayout;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment_HotnNew#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment_HotnNew extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment_HotnNew() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment_HotnNew.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment_HotnNew newInstance(String param1, String param2) {
+        Fragment_HotnNew fragment = new Fragment_HotnNew();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment__hotn_new, container, false);
+
+        TabLayout subcategoryLayout = view.findViewById(R.id.hotNewTabLayout);
+        ViewPager2 subcategoryViewPager = view.findViewById(R.id.hotNewViewPager);
+
+        Hot_New_Pager_Adapter hot_new_pager_adapter = new Hot_New_Pager_Adapter(requireActivity());
+        subcategoryViewPager.setAdapter(hot_new_pager_adapter);
+        return view;
+    }
+}
