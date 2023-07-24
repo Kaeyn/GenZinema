cf13e028fdf3f30fa7434f3aaafe84c615308302
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2023-07-24 09:37:05 +0700

    Added Homepage
    +Folder : Controller, Model, View 🤞

---
+++ app/src/main/java/android/genzinema/View/HomePage.java
@@ -0,0 +1,15 @@
+package android.genzinema.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.genzinema.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
